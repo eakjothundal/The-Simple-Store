4f15614b066a641177e7500c4c51c5d1fc2b47fa
Eakjot Hundal <eakjothundal@gmail.com>
2023-12-09 13:15:15 -0800

    Refactor RemoveFromCartServlet to use CartBean

--- src/main/java/com/shoppingcart/servlets/RemoveFromCartServlet.java
+++ src/main/java/com/shoppingcart/servlets/RemoveFromCartServlet.java
@@ -1,24 +1,25 @@
 package com.shoppingcart.servlets;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import com.shoppingcart.beans.CartBean;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.ServletException;
 import java.io.IOException;
-import java.util.Map;
 
 @WebServlet(name = "RemoveFromCartServlet", urlPatterns = { "/remove-from-cart" })
 public class RemoveFromCartServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
-
-        @SuppressWarnings("unchecked")
-        Map<String, Integer> cart = (Map<String, Integer>) session.getAttribute("cart");
+        CartBean cartBean = (CartBean) session.getAttribute("cartBean");
         String productId = request.getParameter("productId");
 
-        if (cart != null && productId != null) {
-            cart.remove(productId);
-            session.setAttribute("cart", cart);
+        if (cartBean != null && productId != null) {
+            cartBean.removeItem(productId);
+            session.setAttribute("cartBean", cartBean);
         }
 
         response.sendRedirect("cart.jsp");
