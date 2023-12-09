8139f649c75ff29fe4ce258bf2c6437e81474b40
Eakjot Hundal <eakjothundal@gmail.com>
2023-12-09 13:13:47 -0800

    Add CartBean Java Bean to encapsulate cart operations

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,6 +1,4 @@
 
-h
-8src/main/java/com/example/shoppingcart/HelloServlet.java,c/9/c990230357ae2d9c22084a296987cd6243e92bcb
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
@@ -8,12 +6,6 @@ h
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 I
 src/main/webapp/index.jsp,8/e/8e6d3b70c9f5b02570779b0441540d45eceb0ded
-J
-src/main/webapp/styles.css,a/5/a528d543f5b88a435b4532b41af24e79b3ccc297
-i
-9src/main/java/com/example/shoppingcart/model/Product.java,3/1/3177963ac2de4289e95ead9dfb8f3c21a1c71019
-`
-0src/main/java/com/shoppingcart/HelloServlet.java,7/2/727d3bdfbff4ea8ef8e1b658a6f27b8e529bc65e
 a
 1src/main/java/com/shoppingcart/model/Product.java,2/0/20f4a6d8bd03f8cf064dafe902175204e129d94c
 i
@@ -40,3 +32,5 @@ O
 src/main/webapp/styles/cart.css,9/0/902b9609d25921747b6bfcab54c6beb58c5d4cd6
 R
 "src/main/webapp/styles/receipt.css,4/e/4e3779e3a92a857bb0fc5ec3d5c2c9447d1fd689
+b
+2src/main/java/com/shoppingcart/beans/CartBean.java,3/6/36d9b564d699b24f09a304e3437524d4a99d098d
---
+++ src/main/java/com/shoppingcart/beans/CartBean.java
@@ -0,0 +1,51 @@
+package com.shoppingcart.beans;
+
+import com.shoppingcart.model.Product;
+import com.shoppingcart.data.ProductDataStore;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CartBean implements Serializable {
+    private Map<String, Integer> items = new HashMap<>();
+
+    public CartBean() {
+        // No-arg constructor
+    }
+
+    public void addItem(String productId, int quantity) {
+        // If product already exists in cart, update the quantity
+        if (items.containsKey(productId)) {
+            items.put(productId, items.get(productId) + quantity);
+        } else {
+            items.put(productId, quantity);
+        }
+    }
+
+    public void removeItem(String productId) {
+        items.remove(productId);
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
+    public double calculateSubtotal() {
+        double subtotal = 0.0;
+        for (Map.Entry<String, Integer> entry : items.entrySet()) {
+            Product product = ProductDataStore.getProduct(entry.getKey());
+            double price = (product != null) ? product.getPrice() : 0.0;
+            subtotal += (price * entry.getValue());
+        }
+        return subtotal;
+    }
+
+    public Map<String, Integer> getItems() {
+        return this.items;
+    }
+
+    public void setItems(Map<String, Integer> items) {
+        this.items = items;
+    }
+}
