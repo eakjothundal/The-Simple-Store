2df7de52c9a8aaa409c734ea3581ba12c27e4d1e
Eakjot Hundal <eakjothundal@gmail.com>
2023-12-09 13:17:48 -0800

    Refactor CheckoutServlet to use CartBean for checkout process

--- src/main/java/com/shoppingcart/servlets/CheckoutServlet.java
+++ src/main/java/com/shoppingcart/servlets/CheckoutServlet.java
@@ -1,7 +1,6 @@
 package com.shoppingcart.servlets;
 
-import com.shoppingcart.data.ProductDataStore;
-import com.shoppingcart.model.Product;
+import com.shoppingcart.beans.CartBean;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,9 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.util.Map;
 
 @WebServlet(name = "CheckoutServlet", urlPatterns = { "/checkout" })
 public class CheckoutServlet extends HttpServlet {
@@ -20,45 +17,34 @@ public class CheckoutServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Get the shopping cart or create it if it's not in the session yet
         HttpSession session = request.getSession();
+        CartBean cartBean = (CartBean) session.getAttribute("cartBean");
 
-        @SuppressWarnings("unchecked")
-        Map<String, Integer> cart = (Map<String, Integer>) session.getAttribute("cart");
-        if (cart == null) {
+        if (cartBean == null) {
             response.sendRedirect("index.jsp");
             return;
         }
 
-        // Calculate the subtotal for the items in the cart
-        double subtotal = cart.entrySet().stream()
-                .mapToDouble(entry -> {
-                    Product product = ProductDataStore.getProduct(entry.getKey());
-                    return product.getPrice() * entry.getValue();
-                })
-                .sum();
+        double subtotal = cartBean.calculateSubtotal();
 
-        // Apply discounts if a promo code is used (this is simplified)
         String promoCode = request.getParameter("promoCode");
         double discount = 0;
         if (promoCode != null && promoCode.equals("SPECIAL10")) {
-            discount = subtotal * 0.1; // 10% discount for the promo code SPECIAL10
+            discount = subtotal * 0.10; // 10% discount for the promo code SPECIAL10
         }
 
         double tax = (subtotal - discount) * TAX_RATE;
         double total = subtotal - discount + tax + SHIPPING_COST;
 
-        // Pass along the totals to the request (jsp can pull from request scope)
         request.setAttribute("subtotal", subtotal);
         request.setAttribute("discount", discount);
         request.setAttribute("tax", tax);
         request.setAttribute("shippingCost", SHIPPING_COST);
         request.setAttribute("total", total);
 
-        cart.clear();
-        session.setAttribute("cart", cart);
+        cartBean.clearCart();
+        session.setAttribute("cartBean", cartBean);
 
-        // Forward to the JSP page that will show the receipt
         RequestDispatcher dispatcher = request.getRequestDispatcher("receipt.jsp");
         dispatcher.forward(request, response);
     }
